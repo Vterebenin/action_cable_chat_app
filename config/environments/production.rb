1a08466f0aa5f9a4fb86e641fc272a65fc851a7e
Vterebenin <niceserver11@mail.ru>
2018-04-28 16:38:02 +0300

    Prep2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://simple-chat-ternow.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'http://simple-chat-ternow.herokuapp.com', /http:\/\/simple-chat-ternow.herokuapp.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://simple-chat-ternow.herokuapp.com', /http:\/\/simple-chat-ternow.herokuapp.*/ ]
 
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
